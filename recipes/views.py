de1d2df0952c9b2c15595367c3c1b2cf43775702
pliniogim <pliniogimenez@gmail.com>
2024-01-24 21:37:23 -0300

    template

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     """HttpResponse function"""
-    return HttpResponse("Home")
-
+    print(request)
+    # return HttpResponse("Home")
+    return render(request, 'recipes/home.html', context={
+        'name': 'Plínio Gimenez',
+    })
+    # return render(request, 'global/home.html')
 
 def about(request):
     """HttpResponse function"""
+    print(request)
     return HttpResponse("Sobre")
 
 
 def contact(request):
     """HttpResponse function"""
+    print(request)
     return HttpResponse("Contato")

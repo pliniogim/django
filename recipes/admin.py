fbf2b9d22068a4982ca980e38b75467cf04ce5ea
pliniogim <pliniogimenez@gmail.com>
2024-01-28 20:57:37 -0300

    models 1

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+# @admin.register
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)

530f560466d073b61887add4ed258ac60716b309
pliniogim <pliniogimenez@gmail.com>
2024-01-28 15:24:56 -0300

    before models

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from . import views
 # from recipes.views import home, about, contact
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
